4995324b297ff1b790fe16299239c7a65bd835ef
RidwanC134 <ridwanhh10@gmail.com>
2024-12-15 21:30:34 +0700

    Mengambil instance dari database room enggunakan konteks aplikasi. Mengambil akses DAO untuk tabel mahasiswa

--- app/src/main/java/com/dwan/roomdatabase/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/dwan/roomdatabase/dependenciesinjection/ContainerApp.kt
@@ -11,6 +11,6 @@ interface InterfaceContainerApp {
 
 class ContainerApp(private val context: Context) : InterfaceContainerApp {
     override val repositoryMhs: RepositoryMhs by lazy { // Menunda pembuatan objek hingga properti diakses
-        LocalRepositoryMhs(KrsDatabase.getDatabase(context).mahasiswaDao())
+        LocalRepositoryMhs(KrsDatabase.getDatabase(context).mahasiswaDao()) // Mengambil instance dari database room
     }
 }
